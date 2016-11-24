6c586f14790d3b25ca5c0f721b588dfeeb4f75c9
iquirin <iago@ciandt.com>
2016-11-24 20:59:11 -0200

    first commit

---
+++ app/common/components/components.js
@@ -0,0 +1,21 @@
+'use strict';
+
+define(['ng-jedi-i18n',
+        'ng-jedi-dialogs',
+        'ng-jedi-factory',
+        'ng-jedi-loading',
+        'ng-jedi-table',
+        'ng-jedi-layout',
+        'ng-jedi-activities',
+        'app-common-components-exceptions'], function () {
+
+    angular.module('app.common.components', ['jedi.i18n',
+                                             'jedi.dialogs',
+                                             'jedi.factory',
+                                             'jedi.loading',
+                                             'jedi.table',
+                                             'jedi.layout',
+                                             'jedi.activities',
+                                             'app.common.components.exceptions']);
+
+});
---
+++ app/core/features/mysubmodule/myfeature1/myfeature1-ctrl.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/*
+    Controller for the feature myfeature1
+*/
+jd.factory.newController('app.core.mysubmodule.myfeature1.Myfeature1Ctrl', ['jedi.dialogs.AlertHelper', 'toastr', function (alertHelper, toastr) {
+
+    //#region Service initialize
+    var service;// = ... e.g: restangular instance
+    //#endregion
+
+    //#region View/Model initialize
+    var vm = this;
+    vm.child = {};
+    vm.children = [];
+    //#endregion
+
+    //#region Events binds
+    vm.save = save;
+    vm.remove = remove;
+    vm.reset = reset;
+    //#endregion
+
+
+    //#region Events definitions
+    function save() {
+        vm.children.push(vm.child);
+        vm.reset();
+        alertHelper.addInfo('message');
+    }
+
+    function remove(index){
+      vm.children.splice(index,1);
+      alertHelper.addInfo('remove_message');
+    }
+
+    function reset(){
+      vm.child = {}
+    }
+
+}]);
